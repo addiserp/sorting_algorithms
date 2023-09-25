440929b09eb41868e93cff3fc1315072c8c3de82
Mikias Gedlu <addiserp@gmail.com>
2023-09-25 17:36:10 +0300

    0x1B. C - Sorting algorithms & Big O 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ * using the Selection sort algorithm
+ * @array: A list with numbers
+ * @size: A size of the array
+
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t a, index;
+	int tmp, swap, flag = 0;
+
+	if (array == NULL)
+		return;
+	for (a = 0; a < size; a++)
+	{
+		tmp = a;
+		flag = 0;
+		for (index = a + 1; index < size; index++)
+		{
+			if (array[tmp] > array[index])
+			{
+				tmp = index;
+				flag += 1;
+			}
+		}
+		swap = array[a];
+		array[a] = array[tmp];
+		array[tmp] = swap;
+		if (flag != 0)
+			print_array(array, size);
+	}
+}

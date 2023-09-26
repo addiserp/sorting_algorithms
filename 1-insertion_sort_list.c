1ff791b9bd43c4d41f2b4ca88a81414b02075259
Mikias Gedlu <addiserp@gmail.com>
2023-09-26 16:27:25 +0300

    0x1B. C - Sorting algorithms & Big O fix

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 /**
  * insertion_sort_list - using the Insertion sort algorithm
- * @list: To sort the dobule linked list 
+ * @list: To sort the dobule linked list
  */
 
 void insertion_sort_list(listint_t **list)
